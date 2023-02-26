3beaad140887a0e64c178c4e4683575092bb404e
Anthony17 <softech.series@gmail.com>
2023-02-25 20:57:52 -0500

    Actualizacion repo

--- MultiplicacionRecursiva.java
+++ MultiplicacionRecursiva.java
@@ -1,23 +1,9 @@
-class MultiplicacionRecursiva {
-    public static int multiplicar(int a, int b) {
-        if (b == 0) {
-            return 0;
-        } else if (b > 0) {
-            return a + multiplicar(a, b - 1);
+public class MultiplicacionRecursiva {
+    public static int MR(int a, int b) {
+        if (b == 1) { // Caso base
+            return a;
         } else {
-            return -multiplicar(a, -b);
-        }
-    }
-
-    public static String expresarComoSuma(int resultado, int a, int b) {
-        if (resultado == 0) {
-            return "0";
-        } else if (resultado < 0) {
-            return "-" + expresarComoSuma(-resultado, a, b);
-        } else if (resultado == a) {
-            return Integer.toString(a);
-        } else {
-            return "(" + expresarComoSuma(resultado - a, a, b) + ") + " + b;
+            return a + MR(a, b-1); // Llamada recursiva
         }
     }
 }
--- Principal.java
+++ Principal.java
@@ -2,10 +2,8 @@ public class Principal{
 	public static void main(String []args){
 	Estudiante estudiante = new Estudiante("Anthony","Carrascal",20,"Ingles","Software");
 	estudiante.mostrarDatos();
- 	int a = 3;
-        int b = 4;
-        int resultado = MultiplicacionRecursiva.multiplicar(a, b);
-        System.out.println(a + " x " + b + " = " + resultado);
-        System.out.println("El resultado como suma es: " + MultiplicacionRecursiva.expresarComoSuma(resultado, a, b));
+	MultiplicacionRecursiva multi = new MultiplicacionRecursiva();
+	System.out.println("El resultado de una multiplicacion recursiva entre 5 y 4 es: \n"+multi.MR(5, 4));
+
 	}
 }
