a544b0ca6a6f9ad945b5d606e42ce8925466f4eb
Anthony17 <softech.series@gmail.com>
2023-02-27 10:53:28 -0500

    Actualizacion repo

--- Principal.java
+++ Principal.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 public class Principal{
+
+
 	public static void main(String []args){
 	Estudiante estudiante = new Estudiante("Anthony","Carrascal",20,"Ingles","Software");
-	estudiante.mostrarDatos();
 	MultiplicacionRecursiva multi = new MultiplicacionRecursiva();
-	System.out.println("El resultado de una multiplicacion recursiva entre 5 y 4 es: \n"+multi.MR(5, 4));
+	Scanner scanner= new Scanner(System.in);
+
+	
+
+	System.out.println("Trabajos Anthony");
+	System.out.println("1: Estudiante");
+	System.out.println("2: Multiplicacion Recursiva");
+	System.out.println(" Por favor digite la opcion: ");
+	int opcion=scanner.nextInt();
+	switch(opcion){
+		case 1:
+			
+			estudiante.mostrarDatos();
+			break;
+		case 2:
+			
+			System.out.println("El resultado de una multiplicacion recursiva entre 8 y 4 es: \n"+multi.MR(8, 4));
+			break;
+		}
+				
 
 	}
 }
